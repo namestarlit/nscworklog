97ff191250282937cecad5be0d3542750ee12b44
Paul John <rainson.work@gmail.com>
2024-02-25 00:33:09 +0300

    Add webapp user model

--- src/webapp/__init__.py
+++ src/webapp/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_login import LoginManager
 from webapp.configuration import Config
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config.from_object(Config)
+
+login = LoginManager(app)
 
 from webapp.routes import routes
---
+++ src/webapp/models/user.py
@@ -0,0 +1,25 @@
+from webapp import login
+from flask_login import UserMixin
+
+from worklog import storage
+from worklog.user import User
+
+
+@login.user_loader
+def load_user(id):
+    """Load the user into Flask's session"""
+    user = storage.get("users", id)
+    return user
+
+
+class User(UserMixin, User):
+    """User class implementantion"""
+
+    def __init__(self,username, email, password, *args, **kwargs):
+        """Initializes an instance of User class"""
+        super().__init__(username, email, password, *args, **kwargs)
+
+    def avatar(self, size):
+        """Gets a user's profile picture."""
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
