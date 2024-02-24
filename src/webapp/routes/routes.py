6e067618fb364ba21ce2c98bd24422dd3422461b
Paul John <rainson.work@gmail.com>
2024-02-24 22:20:37 +0300

    Add routes.py

--- src/webapp/routes/routes.py
+++ src/webapp/routes/routes.py
@@ -1,7 +1,22 @@
 from webapp import app
+from flask import render_template
 from worklog import storage
 
+
 @app.route("/")
-def index():
+def homepage():
     """homepage"""
-    return "Welcome to Worklog"
+    return "Welcome to nscworklog"""
+
+
+@app.route("/<user_id>")
+def index(user_id):
+    """user homepage"""
+    worklogs = storage.all("worklogs", user_id)
+    worklogs = [work.to_dict() for work in worklogs]
+
+    for worklog in worklogs:
+        for key in ["_id", "created_at", "updated_at", "user_id"]:
+            worklog.pop(key, None)
+
+    return render_template("index.html", worklogs=worklogs)
