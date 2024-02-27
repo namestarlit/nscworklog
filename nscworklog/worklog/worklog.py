e08ef0068cef426f565168a893a2e049f3c5eaab
Paul-Njuga <tyrosavant@gmail.com>
2024-02-27 16:14:56 +0300

    Make description arg optional

--- nscworklog/worklog/worklog.py
+++ nscworklog/worklog/worklog.py
@@ -6,12 +6,13 @@ from worklog.base import Base
 class Worklog(Base):
     """Worklog class implementantion"""
 
-    def __init__(self, title, description, user_id, *args, **kwargs):
+    def __init__(self, title, user_id, description=None, status="pending", *args, **kwargs):
         """Initializes an instance of Resource class"""
         super().__init__(*args, **kwargs)
         self.title = title
         self.description = description
         self.user_id = user_id
+        self.status = status
 
     def __str__(self):
         """String representation of the worklog"""
